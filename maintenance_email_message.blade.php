c59c3638921f6f2f6e7c84ce91280dd3eaed1123
francis-ux <kingmicheal744@gmail.com>
2025-03-14 22:09:57 +0100

    commit

---
+++ maintenance_email_message.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Maintenance Email Message</title>
+    </head>
+
+    <body>
+        <p>
+            We are informing you that your website skylines.efficientconstructs.com will be maintained this Friday.
+            During
+            this period, our team will perform essential updates and checks to ensure that your website runs smoothly
+            and
+            securely.
+
+            <strong>Maintenance Schedule:</strong>
+            <strong>Date: Friday, March 14, 2025</strong>
+            <strong>Time: 10:00 AM to 2:00 PM WAT</strong>
+
+            <strong>Maintenance Fee: $20</strong>
+
+            We appreciate your understanding and cooperation. If you have any questions or concerns, please contact our
+            support team.
+
+            Thank you for choosing our services. We look forward to continuing to support your online presence.
+        </p>
+    </body>
+
+</html>

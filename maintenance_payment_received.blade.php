ab4a424fea220586f94760dfac4a344b1d653e78
francis-ux <kingmicheal744@gmail.com>
2025-08-11 12:59:08 +0100

    commit

---
+++ maintenance_payment_received.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Live Chat Disabled Message</title>
+    </head>
+
+    <body>
+        <p>
+            We have received your payment for the maintenance of nova.astrabnk.com. Thank you for your prompt response.
+
+            Our team is now working on the necessary updates, and the website will be up and running shortly.
+
+            If you have any additional requests or inquiries, please feel free to reach out.
+        </p>
+    </body>
+
+</html>

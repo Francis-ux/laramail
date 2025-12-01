4655cd3351d5237afd6c107ed23e5b8d9a1db579
francis-ux <kingmicheal744@gmail.com>
2025-12-02 00:19:51 +0100

    timezone changed

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -36,7 +36,7 @@ class MailController extends Controller
         ];
 
         try {
-            Mail::to($request->receiver_email)->send(new Message($emailData, $request->subject));
+            Mail::to($request->receiver_email)->send(new Message($emailData, $request->subject . ' - ' . now()));
         } catch (\Exception $e) {
             return back()->with('error', $e->getMessage());
         }
